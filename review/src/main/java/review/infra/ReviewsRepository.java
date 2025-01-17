7bc8d660e81b7661dd4eab7349a1db7fc634dabf
dhtns818 <dhtns818@rosesystems.kr>
2025-01-17 17:26:49 +0900

    commit

--- review/src/main/java/review/infra/ReviewsRepository.java
+++ review/src/main/java/review/infra/ReviewsRepository.java
@@ -1,12 +1,12 @@
-package review.domain;
+package review.infra;
 
+import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import review.domain.*;
-import java.util.List;
 
-
-//<<< PoEAA / Repository
 @RepositoryRestResource(collectionResourceRel = "reviews", path = "reviews")
-public interface ReviewRepository
-    extends PagingAndSortingRepository<Review, Long> {}
+public interface ReviewsRepository
+    extends PagingAndSortingRepository<Reviews, Long> {
+        List<Reviews> findByItemId(String itemId);
+    }
